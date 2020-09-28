08c3d20bf42052b08beb6d138ae038212fd1d99e
Dant86 <vedantdpathak@iCloud.com>
2020-09-28 00:05:43 -0500

    second nosetest in

--- nlw_generator/formations/tests/test_model.py
+++ nlw_generator/formations/tests/test_model.py
@@ -40,9 +40,31 @@ class Test_IsNLW_Valid(TestCase):
 
 class Test_IsNLW_Invalid(TestCase):
 
-    def setup(self):
+    def setUp(self):
         self.players = read_csv('players.csv')
         self.formation = Formation('4332.form')
     
     def test_invalid_nlw(self):
-        pass
+        self.formation.insert(0, by_name(self.players, \
+                                         'Alphonse Areola'))
+        self.formation.insert(1, by_name(self.players, \
+                                         'Theo Hernández'))
+        self.formation.insert(2, by_name(self.players, \
+                                         'Lucas Hernández'))
+        self.formation.insert(3, by_name(self.players, \
+                                         'Dayot Upamecano'))
+        self.formation.insert(4, by_name(self.players, \
+                                         'Kévin Malcuit'))
+        self.formation.insert(5, by_name(self.players, \
+                                         'Arturo Vidal'))
+        self.formation.insert(6, by_name(self.players, \
+                                         'Marcelo Brozović'))
+        self.formation.insert(7, by_name(self.players, \
+                                         'Radja Nainggolan'))
+        self.formation.insert(8, by_name(self.players, \
+                                         'Antoine Griezmann'))
+        self.formation.insert(9, by_name(self.players, \
+                                         'João Félix'))
+        self.formation.insert(10, by_name(self.players, \
+                                          'Ángel Correa'))
+        self.assertFalse(self.formation.is_nlw())
